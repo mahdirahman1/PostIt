37fee18b4fba20097d6c63a9de892b784247ddb5
Mahdi Rahman <35085896+mahdirahman1@users.noreply.github.com>
2021-09-05 20:33:24 -0400

    Edit site key for firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -21,7 +21,7 @@ const appCheck = firebase.appCheck();
 // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
 // key is the counterpart to the secret key you set in the Firebase console.
 appCheck.activate(
-  KEY,
+  "6LcjkEkcAAAAAP4-wgrhb0V5K-6NREXAofRw-8dQ",
 
   // Optional argument. If true, the SDK automatically refreshes App Check
   // tokens as needed.
