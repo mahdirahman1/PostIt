b011cd6c34b3718b1cb46f9f6b67fb94f54e6ef9
Mahdi Rahman <35085896+mahdirahman1@users.noreply.github.com>
2021-09-05 20:25:11 -0400

    Add app check

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/storage";
+import "firebase/app-check";
 const API_KEY = process.env.REACT_APP_FIREBASE_API;
 const KEY = process.env.REACT_APP_SECRET_KEY;
 
