e20ca1f71bdbc41339024dbce5fb5f489e399b44
Mahdi Rahman <35085896+mahdirahman1@users.noreply.github.com>
2021-09-05 20:23:34 -0400

    Add app check

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 import "firebase/storage";
 const API_KEY = process.env.REACT_APP_FIREBASE_API;
+const KEY = process.env.REACT_APP_SECRET_KEY;
 
 var firebaseConfig = {
   apiKey: API_KEY,
@@ -14,6 +15,17 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+const appCheck = firebase.appCheck();
+// Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// key is the counterpart to the secret key you set in the Firebase console.
+appCheck.activate(
+  KEY,
+
+  // Optional argument. If true, the SDK automatically refreshes App Check
+  // tokens as needed.
+  true
+);
 
 const firebaseStorage = firebase.storage();
 const firestore = firebase.firestore();
